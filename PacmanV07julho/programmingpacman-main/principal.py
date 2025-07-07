ff3eba9135000caf2ab0552012742360b9da0734
Victoria <rebeca_rodrigues@estudante.sesisenai.org.br>
2025-07-07 12:22:50 -0300

    materiais aula 7 julho
    upload arquivos 7 julho

---
+++ PacmanV07julho/programmingpacman-main/principal.py
@@ -0,0 +1,59 @@
+import pygame 
+import Constantes
+import Sprites 
+
+class game: 
+    def _init_(self):
+        #criando a tela do jogo
+        pygame.init()
+        pygame.mixer.init()
+        self.tela = pygame.display.set_mode((Constantes.LARGURA, Constantes.ALTURA))
+        pygame.display.set_caption(Constantes.TITULO_JOGO)
+        self.relogio = pygame.time.Clock()
+        self.esta_rodando = true 
+
+    def novo_jogo(self):
+        #instancia as classes das sprintes do jogo 
+        self.todas_as_sprites = pygame.sprite.group()
+        self.rodar()
+
+    def rodar(self)
+        #loop do jogo 
+        self.jogando = True
+        while self.jogando:
+            self.relogio.tick(Constantes.FPS)
+            self.eventos()
+            self.atualizar_sprites()
+            self.desenhar_sprites()
+
+    def eventos(self):
+        #define os eventos do jogo
+        for event in pygame.event.get():
+                If event.type == QUIT:
+                    If self.jogando:
+                        self.jogando = False
+                    self.esta_rodando = False 
+  
+    def atualizar_sprites(self):
+        #atualizar sprites
+        self.todas_as_sprites.update()
+
+    def desenhar_sprites(self):
+        #desenhar sprites
+        self.tela.fill(Constantes.PRETO) #limpando a tela 
+        self.todas_as_sprites.draw(self.tela) # desenhando as sprites 
+        pygame.display.flip()
+
+    def mostrar_tela_start(self):
+            pass
+    
+    def mostrar_tela_game_over(self):
+            pass 
+    
+    g = game()
+    g.mostrar_tela_de_start()
+
+while g.esta_rodando:
+     g.novo_jogo()
+     g.mostrar_tela_game_over()
+     
